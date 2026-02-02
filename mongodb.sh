3536249ca42a2741b632817a03ed5162dec8c80c
zakeer3551 <zak.ican4d@gmail.com>
2026-02-02 19:36:35 +0530

    deploying roboshop using shell script

--- mongodb.sh
+++ mongodb.sh
@@ -2,16 +2,16 @@
 
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOG_FOLDER/$0.log"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 if [ $USER_ID -ne 0 ]; then
-    echo "Please run the script with Root user" | tee -a $LOGS_FILE
+    echo "$R Please run the script with Root user $N" | tee -a $LOGS_FILE
     exit 1
-elseif 
+else
     echo "Running the script with Root user" | tee -a $LOGS_FILE
 fi    
 
@@ -20,7 +20,7 @@ mkdir -p $LOGS_FOLDER
 VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... :: $R FAILED $N" | tee -a $LOGS_FILE
-    elseif 
+    else 
         echo -e "$2 ... :: $R SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
