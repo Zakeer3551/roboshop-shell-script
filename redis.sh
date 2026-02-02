eda2c95fadf5ae72cff99cb6ecdaa332ae528f3c
zakeer3551 <zak.ican4d@gmail.com>
2026-02-02 22:53:24 +0530

    deploying roboshop using shell script

---
+++ redis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USER_ID -ne 0 ]; then
+    echo "$R Please run the script with Root user $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo "$G Running the script with Root user $N" | tee -a $LOGS_FILE
+fi    
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... :: $R FAILED $N" | tee -a $LOGS_FILE
+    else 
+        echo -e "$2 ... :: $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+
+VALIDATE $? "Disabling the existing redis 6 module and Enabling redis 7 module"
+
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i '/s/127.0.0.1/0.0.0.0/g' -e '/proctected mode / c protected mode no' /etc/redis/redis.conf &>>$LOGS_FILE
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis 
+VALIDATE $? "Enabled and started Redis"
